e4cbf1aa4ca1b2914d00567da408628a391002a4
ineswilms <i.wilms@maastrichtuniversity.nl>
2021-07-07 13:40:12 +0200

    taglasso with or without refitting

--- R/taglasso.R
+++ R/taglasso.R
@@ -3,21 +3,24 @@
 #' @description This function computes the tag-lasso estimator for fixed tuning parameters lambda1 and lambda2
 #' @param X An (\eqn{n}x\eqn{p})-matrix of \eqn{p} variables and \eqn{n} observations
 #' @param A An (\eqn{p}x\eqn{|T|})- binary matrix incorporating the tree-based aggregation structure
-#' @param pendiag Logical indicator whether or not to penalize the diagonal in Omega. The default is \code{TRUE} (penalization of the diagonal)
+#' @param pendiag Logical indicator whether or not to penalize the diagonal in Omega. The default is \code{FALSE} (no penalization of the diagonal)
 #' @param lambda1 Aggregation tuning parameter. Use \code{taglasso_cv} to let the program determine this tuning parameter based on K-fold cross-validation
 #' @param lambda2 Sparsity tuning parameter. Use \code{taglasso_cv} to let the program determine this tuning parameter based on K-fold cross-validation
 #' @param rho Starting value for the LA-ADMM tuning parameter. Default is 10^2; will be locally adjusted via LA-ADMM
 #' @param it_in Number of inner stages of the LA-ADMM algorithm. Default is 100
 #' @param it_out Number of outer stages of the LA-ADMM algorithm. Default is 10
-#' @param it_in_refit Number of inner stages of the LA-ADMM algorithm for re-fitting. Default is 100
-#' @param it_out_refit Number of outer stages of the LA-ADMM algorithm for re-fitting. Default is 10
+#' @param refitting Logical indicator whether refitting subject to sparsity and aggregation constraints is done or not. The default is \code{TRUE}.
+#' @param it_in_refit Number of inner stages of the LA-ADMM algorithm for re-fitting (Only relevant if \code{refitting = TRUE}). Default is 100
+#' @param it_out_refit Number of outer stages of the LA-ADMM algorithm for re-fitting (Only relevant if \code{refitting = TRUE}). Default is 10
+
 #' @return A list with the following components
 #' \item{\code{omega_full}}{Estimated (\eqn{p}x\eqn{p}) precision matrix}
-#' \item{\code{omega_aggregated}}{Estimated (\eqn{K}x\eqn{K}) precision matrix}
 #' \item{\code{cluster}}{Numeric vector indicating the cluster groups for each of the \eqn{p} original variables}
-#' \item{\code{M}}{The (\eqn{p}x\eqn{K} membership matrix)}
+#' \item{\code{sparsity}}{The (\eqn{p}x\eqn{p} matrix indicating the sparsity pattern in Omega (1=non-zero, 0=zero))}
+#' \item{\code{fit}}{Fitted object from LA_ADMM_taglasso_export cpp function, for internal use now}
+#' \item{\code{refit}}{Fitted object from refit_LA_ADMM_export cpp function, for internal use now}
 taglasso <- function(X, A, pendiag = F,  lambda1, lambda2,
-                     rho = 10^-2, it_in = 100, it_out = 10,  it_in_refit = 100, it_out_refit = 10){
+                     rho = 10^-2, it_in = 100, it_out = 10, refitting = T,  it_in_refit = 100, it_out_refit = 10){
 
   #### Preliminaries ####
   # Dimensions
@@ -45,8 +48,10 @@ taglasso <- function(X, A, pendiag = F,  lambda1, lambda2,
     Z <- nrow(fit_taglasso$gam1)
   }
   AZ <- A[, Z]
+  gam1_red <- fit_taglasso$gam1[Z, ]
   if(length(Z)==1){
     AZ = matrix(AZ, p, 1)
+    gam1_red = matrix(gam1_red, 1, p)
   }
   unique_rows <- unique(AZ)
   K <- nrow(unique_rows) # Number of nodes in aggregated network
@@ -59,57 +64,59 @@ taglasso <- function(X, A, pendiag = F,  lambda1, lambda2,
 
   #### Level of sparsity ####
   om_P <- (fit_taglasso$om3!=0)*1 # 1 if non-zero
-
-
-  ##### Refit subject to aggregation and sparsity constraints of tag-lasso ####
-  gaminit_refit <- matrix(0, ncol(AZ), nrow(AZ))
-  prelims_refit <- preliminaries_for_refit_in_R(AZ)
-  refit <- refit_LA_ADMM_export(it_out = it_out_refit, it_in = it_in_refit, S = S,
-                                A =  AZ,  Atilde = prelims_refit$Atilde, A_for_gamma = prelims_refit$A_for_gamma,
-                                A_for_B = prelims_refit$A_for_B, C = prelims_refit$C, C_for_D = prelims_refit$C_for_D,
-                                omP = om_P, rho = rho,
-                                init_om = ominit, init_u1 = ominit, init_u2 = ominit, init_u3 = ominit,
-                                init_gam = gaminit_refit, init_u4 = gaminit_refit, init_u5 = gaminit_refit)
-
-
-  #### Full and Aggregated Precision Matrix  ####
-  omega_block <- AZ%*%refit$gam1
-  d <- diag(refit$D)
-  re_order <- c()
-  M <- matrix(0, p, K) # Membership matrix
-  rownames(M) <- colnames(X)
-  for(i.c in 1:K){
-    re_order <- c(re_order, which(cluster==i.c))
-    M[which(cluster==i.c), i.c] = 1
+  if(pendiag==F){
+    diag(om_P) <- 1 # Diagonal elements are also estimated and are in D
   }
-  omega_block_re_order <- omega_block[re_order, re_order]
-  omega_block_re_order[lower.tri(omega_block_re_order)] <- t(omega_block_re_order)[lower.tri(omega_block_re_order)]
-  colnames(omega_block_re_order) <- rep("", ncol(omega_block_re_order))
-  nbr_variables <- 0
-  counter <- 1
-  # d_agg <- rep(NA, K)
-  for(i in 1:K){
-    colnames(omega_block_re_order)[1+nbr_variables] <- counter
-    nbr_variables <- nbr_variables + length(which(sort(cluster)==i))
-    # d_agg[i] <- mean(d[which(sort(cluster)==i)])
-    counter = counter + 1
+
+  if(refitting){
+    ##### Refit subject to aggregation and sparsity constraints of tag-lasso ####
+    gaminit_refit <- matrix(0, ncol(AZ), nrow(AZ))
+    prelims_refit <- preliminaries_for_refit_in_R(AZ)
+    refit <- refit_LA_ADMM_export(it_out = it_out_refit, it_in = it_in_refit, S = S,
+                                  A =  AZ,  Atilde = prelims_refit$Atilde, A_for_gamma = prelims_refit$A_for_gamma,
+                                  A_for_B = prelims_refit$A_for_B, C = prelims_refit$C, C_for_D = prelims_refit$C_for_D,
+                                  omP = om_P, rho = rho,
+                                  init_om = ominit, init_u1 = ominit, init_u2 = ominit, init_u3 = ominit,
+                                  init_gam = gaminit_refit, init_u4 = gaminit_refit, init_u5 = gaminit_refit)
+
+    omega_full <- refit$om1
+
+    # CODE TO RE-ORDER OMEGA TO BETTER VISUALIZE the CLUSTERS
+    #### Full and Aggregated Precision Matrix  ####
+    # omega_block <- AZ%*%refit$gam1 # gam 1 : Groupwise soft-thresholding
+    # rownames(omega_block) <- colnames(omega_block) <- paste0("p", 1:p)
+    # d <- diag(refit$D)
+    # re_order <- c()
+    # M <- matrix(0, p, K) # Membership matrix
+    # rownames(M) <- colnames(X)
+    # for(i.c in 1:K){
+    #   re_order <- c(re_order, which(cluster==i.c))
+    #   M[which(cluster==i.c), i.c] = 1
+    # }
+
+    # omega_block_re_order <- omega_block[re_order, re_order]
+    # omega_block_re_order[lower.tri(omega_block_re_order)] <- t(omega_block_re_order)[lower.tri(omega_block_re_order)]
+    # colnames(omega_block_re_order) <- rep("", ncol(omega_block_re_order))
+    # nbr_variables <- 0
+    # counter <- 1
+    # # d_agg <- rep(NA, K)
+    # for(i in 1:K){
+    #   colnames(omega_block_re_order)[1+nbr_variables] <- counter
+    #   nbr_variables <- nbr_variables + length(which(sort(cluster)==i))
+    #   # d_agg[i] <- mean(d[which(sort(cluster)==i)])
+    #   counter = counter + 1
+    # }
+    # om_hat_full <- round(omega_block_re_order, 2) + diag(d[re_order])
+    # rownames(om_hat_full) <- colnames(X)
+    # colnames(om_hat_full) <- colnames(omega_block_re_order)
+
+  }else{
+    omega_full <- fit_taglasso$om1
+    refit <- NULL
   }
-  om_hat_full <- round(omega_block_re_order, 2) + diag(d[re_order])
-  rownames(om_hat_full) <- colnames(X)
-  colnames(om_hat_full) <- colnames(omega_block_re_order)
 
-  omega_block[lower.tri(omega_block)] <- t(omega_block)[lower.tri(omega_block)]
-  omega_aggregated <- MASS::ginv(M)%*%round(omega_block, 4)%*%MASS::ginv(t(M))
-  D_agg <- solve(t(M)%*%solve(refit$D)%*%M)
-  # om_hat_agg <- omega_aggregated + diag(d_agg)
-  om_hat_agg <- omega_aggregated + D_agg
-  rownames(om_hat_agg) <- paste0("cluster", 1:K)
-  colnames(om_hat_agg) <- 1:K
-
-
-
-  out <- list("omega_full" = om_hat_full, "omega_aggregated" = om_hat_agg,
-              "cluster" = cluster, "M" = M)
+  out <- list("omega_full" = omega_full, "cluster" = cluster, "sparsity" = om_P, "fit" = fit_taglasso,
+              "refit" = refit)
 }
 
 
