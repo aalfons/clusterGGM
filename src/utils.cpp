881424040a7fc1c67a76f8ced318097ae91361bd
djwtouw <75936485+djwtouw@users.noreply.github.com>
2023-06-29 12:07:04 +0200

    Update utils.cpp

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,6 +1,49 @@
 #include <RcppEigen.h>
 
 
+void printVector(const Eigen::VectorXd& vec)
+{
+    Rcpp::Rcout << '[';
+
+    for (int i = 0; i < vec.size() - 1; i++) {
+        Rcpp::Rcout << vec(i) << ' ';
+    }
+
+    Rcpp::Rcout << vec(vec.size() - 1) << "]\n";
+}
+
+
+void printVector(const Eigen::VectorXi& vec)
+{
+    Rcpp::Rcout << '[';
+
+    for (int i = 0; i < vec.size() - 1; i++) {
+        Rcpp::Rcout << vec(i) << ' ';
+    }
+
+    Rcpp::Rcout << vec(vec.size() - 1) << "]\n";
+}
+
+
+void printMatrix(const Eigen::MatrixXd& mat)
+{
+    Rcpp::Rcout << '[';
+
+    for (int i = 0; i < mat.rows(); i++) {
+        if (i != 0) Rcpp::Rcout << ' ';
+
+        for (int j = 0; j < mat.cols() - 1; j++) {
+            if (mat(i, j) >= 0) Rcpp::Rcout << ' ';
+
+            Rcpp::Rcout << mat(i, j) << ' ';
+        }
+
+        if (i == mat.rows() - 1) Rcpp::Rcout << mat(i, mat.cols() - 1) << "]\n";
+        else Rcpp::Rcout << mat(i, mat.cols() - 1) << '\n';
+    }
+}
+
+
 // [[Rcpp::export]]
 Eigen::MatrixXd updateInverse(const Eigen::MatrixXd& inverse, const Eigen::VectorXd& vec, int i)
 {
@@ -19,6 +62,9 @@ Eigen::MatrixXd updateInverse(const Eigen::MatrixXd& inverse, const Eigen::Vecto
     D = 1.0 / (1.0 + Au(i));
 
     result -= D * N;
+
+    printVector(Au);
+    printMatrix(result);
 
     return result;
 }
